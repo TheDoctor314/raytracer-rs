094f5409ea84c77e407c3a407a332da3a8edd0e2
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2022-07-12 01:11:24 +0530

    Impl add and sub ops on `Vec3` and `Point3`

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 use approx::{AbsDiffEq, RelativeEq};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -15,6 +17,81 @@ pub struct Vec3([f32; 4]);
 impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Self([x, y, z, 0.0])
+    }
+}
+
+// Vec3 + Vec3 -> Vec3
+impl ops::Add for Vec3 {
+    type Output = Self;
+    fn add(mut self, rhs: Self) -> Self::Output {
+        let lhs = &mut self.0;
+        let rhs = &rhs.0;
+
+        for (lhs, rhs) in lhs.iter_mut().zip(rhs.iter()) {
+            *lhs += rhs;
+        }
+
+        Self(*lhs)
+    }
+}
+
+// Point3 + Vec3 -> Point3
+impl ops::Add<Vec3> for Point3 {
+    type Output = Point3;
+    fn add(mut self, rhs: Vec3) -> Self::Output {
+        let lhs = &mut self.0;
+        let rhs = &rhs.0;
+
+        for (lhs, rhs) in lhs.iter_mut().zip(rhs.iter()) {
+            *lhs += rhs;
+        }
+
+        Self(*lhs)
+    }
+}
+
+// Point3 - Point3 -> Vec3
+impl ops::Sub for Point3 {
+    type Output = Vec3;
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        let lhs = &mut self.0;
+        let rhs = &rhs.0;
+
+        for (lhs, rhs) in lhs.iter_mut().zip(rhs.iter()) {
+            *lhs -= rhs;
+        }
+
+        Vec3(*lhs)
+    }
+}
+
+// Point3 - Vec3 -> Point3
+impl ops::Sub<Vec3> for Point3 {
+    type Output = Self;
+    fn sub(mut self, rhs: Vec3) -> Self::Output {
+        let lhs = &mut self.0;
+        let rhs = &rhs.0;
+
+        for (lhs, rhs) in lhs.iter_mut().zip(rhs.iter()) {
+            *lhs -= rhs;
+        }
+
+        Self(*lhs)
+    }
+}
+
+// Vec3 - Vec3 -> Vec3
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        let lhs = &mut self.0;
+        let rhs = &rhs.0;
+
+        for (lhs, rhs) in lhs.iter_mut().zip(rhs.iter()) {
+            *lhs -= rhs;
+        }
+
+        Self(*lhs)
     }
 }
 
