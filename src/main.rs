33563413f6a036adf4033ed4d9d1be49d3796d2a
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2022-09-04 19:05:35 +0530

    Render a basic sphere silhouette

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,36 @@
-use raytracer_rs::{matrix::Mat4, vec3::Point3};
+use raytracer_rs::{ray::Ray, sphere::Sphere, vec3::Point3};
 
 fn main() {
-    let width = 800;
-    let height = 800;
+    let width = 400;
+    let height = 400;
     let mut canvas = image::Rgb32FImage::new(width, height);
     let pixel: image::Rgb<f32> = [1.0, 0.0, 0.0].into();
 
-    let center = ((width / 2) as f32, (height / 2) as f32);
-    let radius = (width as f32) / 4.0;
-    let convert_coords = |x: f32, z: f32| -> (u32, u32) {
-        let x = x * radius;
-        let y = z * radius;
+    let wall_z = 10.0;
+    let wall_size = 7.0;
+    let half_wall = wall_size / 2.0;
 
-        let (x, y) = (x + center.0, y + center.1);
-        (x as u32, y as u32)
-    };
+    let pixel_size = wall_size / width as f32;
+    let s = Sphere::new();
+    let ray_origin = Point3::new(0.0, 0.0, -5.0);
 
-    let mut hours = 12;
+    for y in 0..width {
+        let world_y = half_wall - pixel_size * y as f32;
 
-    let mut p: Point3 = (0., 0., 1.).into();
-    let transform = Mat4::new_rotation_y(std::f32::consts::FRAC_PI_6);
+        for x in 0..height {
+            let world_x = -half_wall + pixel_size * x as f32;
+            let pos = Point3::new(world_x, world_y, wall_z);
+            let ray = Ray::new(ray_origin, (pos - ray_origin).normalize());
 
-    while hours > 0 {
-        let (x, y) = convert_coords(p.x(), p.z());
-        canvas.put_pixel(x, y, pixel);
+            let mut xs = s.intersect(&ray);
 
-        p = &transform * p;
-        hours -= 1;
+            if xs.hit().is_some() {
+                canvas.put_pixel(x, y, pixel)
+            }
+        }
     }
 
     let canvas = image::DynamicImage::ImageRgb32F(canvas).to_rgb8();
 
-    canvas.save("clock.png").unwrap();
+    canvas.save("ch5.png").unwrap();
 }
