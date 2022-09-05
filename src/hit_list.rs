6eee38590a8071fd8c453f0c2a5825131f1aa1dd
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2022-09-05 22:30:04 +0530

    Add `HitState`
    This encapsulates the data required for different calculations in an
    intersection.

--- src/hit_list.rs
+++ src/hit_list.rs
@@ -3,7 +3,11 @@
 
 use std::ops;
 
-use crate::sphere::Sphere;
+use crate::{
+    ray::Ray,
+    sphere::Sphere,
+    vec3::{Point3, Vec3},
+};
 
 /// Stores data related to intersections.
 #[derive(Debug, PartialEq)]
@@ -12,6 +16,34 @@ pub struct HitRec<'a> {
     pub t: f32,
     /// The object of intersection.
     pub obj: &'a Sphere,
+}
+
+impl<'a> HitRec<'a> {
+    /// Constructs a `HitState` to make it easier to reuse computations
+    /// for an intersection.
+    pub fn prepare_computations(&self, r: &Ray) -> HitState<'_> {
+        let t = self.t;
+        let obj = self.obj;
+        let point = r.pos(t);
+        let eyev = -r.dir;
+        let mut normal = obj.normal_at(point);
+
+        let inside = if normal.dot(eyev) < 0.0 {
+            normal = -normal;
+            true
+        } else {
+            false
+        };
+
+        HitState {
+            t,
+            obj,
+            point,
+            eyev,
+            normal,
+            inside,
+        }
+    }
 }
 
 /// A collection of `HitRec`s.
@@ -66,4 +98,16 @@ impl<'a> ops::IndexMut<usize> for HitList<'a> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.inner[index]
     }
+}
+
+/// Stores some data for an intersection which can be reused in
+/// different calculations.
+#[derive(Debug)]
+pub struct HitState<'a> {
+    pub t: f32,
+    pub obj: &'a Sphere,
+    pub point: Point3,
+    pub eyev: Vec3,
+    pub normal: Vec3,
+    pub inside: bool,
 }
--- src/tests.rs
+++ src/tests.rs
@@ -29,3 +29,52 @@ fn color() {
 
     assert_relative_eq!(a.blend(b), [0.9, 0.2, 0.04].into());
 }
+
+mod hit_list {
+    use crate::{hit_list::HitRec, ray::Ray, sphere::Sphere};
+
+    #[test]
+    fn precompute() {
+        let r = Ray::new((0., 0., -5.), (0., 0., 1.));
+        let s = Sphere::default();
+
+        let hit = HitRec { t: 4.0, obj: &s };
+
+        let hit_state = hit.prepare_computations(&r);
+
+        assert_eq!(hit_state.t, hit.t);
+        assert!(std::ptr::eq(hit_state.obj, hit.obj));
+        assert_eq!(hit_state.point, (0., 0., -1.).into());
+        assert_eq!(hit_state.eyev, (0., 0., -1.).into());
+        assert_eq!(hit_state.normal, (0., 0., -1.).into());
+    }
+
+    #[test]
+    fn hit_from_outside_the_sphere() {
+        let r = Ray::new((0., 0., -5.), (0., 0., 1.));
+        let s = Sphere::default();
+
+        let hit = HitRec { t: 4.0, obj: &s };
+
+        let hit_state = hit.prepare_computations(&r);
+
+        assert!(!hit_state.inside);
+    }
+
+    #[test]
+    fn hit_from_inside_the_sphere() {
+        let r = Ray::new((0., 0., 0.), (0., 0., 1.));
+        let s = Sphere::default();
+
+        let hit = HitRec { t: 1.0, obj: &s };
+
+        let hit_state = hit.prepare_computations(&r);
+
+        assert!(hit_state.inside);
+        assert_eq!(hit_state.point, (0., 0., 1.).into());
+        assert_eq!(hit_state.eyev, (0., 0., -1.).into());
+
+        // would have (0., 0., 1.) but is the opposite
+        assert_eq!(hit_state.normal, (0., 0., -1.).into());
+    }
+}
