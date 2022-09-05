2b1d699bc571f3ef279c100b7c05941dfc8c0372
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2022-09-05 15:24:00 +0530

    Remove `Deref` impl from `HitList` and delegate
    We instead delegate all the methods we need. Also `into_inner` allows us
    to get the vector back.

--- src/hit_list.rs
+++ src/hit_list.rs
@@ -1,7 +1,7 @@
 //! This module is responsible for storing the data related to the intersection
 //! of objects with the rays.
 
-use std::ops::Deref;
+use std::ops;
 
 use crate::sphere::Sphere;
 
@@ -20,23 +20,50 @@ pub struct HitList<'a> {
     inner: Vec<HitRec<'a>>,
 }
 
-impl<'a> Deref for HitList<'a> {
-    type Target = Vec<HitRec<'a>>;
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-
 impl<'a> HitList<'a> {
     /// Creates a new `HitList`.
     pub fn new(recs: Vec<HitRec<'a>>) -> Self {
         Self { inner: recs }
     }
 
-    /// Returns the record with the smallest non-negative `t`.
-    pub fn hit(&mut self) -> Option<&HitRec<'_>> {
+    /// Gets the inner vector from the `HitList`.
+    pub fn into_inner(self) -> Vec<HitRec<'a>> {
+        self.inner
+    }
+
+    /// Sorts the list of `HitRec`s.
+    pub fn sort(&mut self) {
         self.inner
             .sort_unstable_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+    }
+
+    /// Returns the record with the smallest non-negative `t`.
+    pub fn hit(&mut self) -> Option<&HitRec<'_>> {
+        self.sort();
         self.inner.iter().find(|h| h.t >= 0.0)
+    }
+
+    /// Returns the number of `HitRec`s stored.
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+
+    /// Returns `true` if the list is empty.
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.inner.is_empty()
+    }
+}
+
+impl<'a> ops::Index<usize> for HitList<'a> {
+    type Output = HitRec<'a>;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.inner[index]
+    }
+}
+
+impl<'a> ops::IndexMut<usize> for HitList<'a> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.inner[index]
     }
 }
